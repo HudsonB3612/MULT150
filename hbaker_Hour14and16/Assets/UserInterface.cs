3dba1a4ab903f3fd05ba6314d88ef9c8ef368475
Hudsonb3612 <hudsonb3612@gmail.com>
2025-05-11 16:34:59 -0700

    Hour14

---
+++ hbaker_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,22 @@
+/*
+ Overall theme is space sci-fi
+ 
+ MenuPanel background set to "BackgroundSpace.png"
+    
+ Changed title text color to #75E6FF
+ Changed subtitle text color to #79FFF1
+ Changed button text color to a horizontal gradient between #75E6FF & #79FFF1
+    
+ Used "Sterion" font (https://www.fontspace.com/sterion-font-f113971) for all text to fit the sci-fi theme
+    
+ TitleText size 72, Bold
+ SubtitleText size 36, Normal
+ StartButton text size 19, Normal
+    
+ Normal color: 75C1FF, 135A
+ Highlighted color: 008CFF, 135A
+ Pressed color: 008CFF, 255A
+    
+ Game Title is "SPACE FIGHTER"
+ Subtitle is "DEFEND YOUR GALAXY"
+*/
