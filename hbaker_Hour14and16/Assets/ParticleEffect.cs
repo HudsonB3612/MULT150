2260d3bb88b582c43b70abba99f70ec8de8f766d
Hudsonb3612 <hudsonb3612@gmail.com>
2025-05-11 17:12:34 -0700

    hour 16

---
+++ hbaker_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,13 @@
+/*
+ "DrippingLavaParticles" Particle Effect
+ 
+ Changed shape to sphere from cone
+ changed shape radius to 2
+ 
+ Changed start color to #FF2B00 (lava like orange)
+ Set gravity modifier to 0.2
+ Set emission rate over time to 30
+ 
+ Implemented the color over time to end with a transparent color
+ Created a size over time for the effect to shrink over time in a non linear curve
+*/
